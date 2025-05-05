93d30ce77c453d2fe8d65cc1fef7a766ff2d062b
T1r4m1su <117399429+T1r4m1su@users.noreply.github.com>
2025-05-05 11:14:47 +0200

    mi

---
+++ lib/model/persona.g.dart
@@ -0,0 +1,19 @@
+part of 'persona.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Persona _$PersonaFromJson(Map<String, dynamic> json) => Persona(
+  id: (json['id'] as num?)?.toInt(),
+  nome: json['nome'] as String,
+  cognome: json['cognome'] as String,
+  mail: json['mail'] as String,
+);
+
+Map<String, dynamic> _$PersonaToJson(Persona instance) => <String, dynamic>{
+  'id': instance.id,
+  'year': instance.nome,
+  'section': instance.cognome,
+  'major': instance.mail,
+};
